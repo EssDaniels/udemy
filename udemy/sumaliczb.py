a4424d4727125cfd0ddffcdfca17637e869ee2fd
EssDaniels <skibol2@wp.pl>
2020-08-21 21:43:38 +0200

    kurs

---
+++ udemy/sumaliczb.py
@@ -0,0 +1,44 @@
+import time
+
+a = int(input("Podaj liczbe do której mają być sumowane "))
+
+
+
+def sumuj_do(number):
+
+    suma = 0
+
+    for number in range(number+1):
+            suma = suma + number
+
+    return suma
+
+def sumuj_do2(number):
+
+    return sum([number for number in range(number+1)])
+def sumuj_do3(number):
+
+    return sum({number for number in range(number+1)})
+def sumuj_do4(number):
+
+    return sum((number for number in range(number+1)))
+
+def sumuj_do5(number):
+    return (1+number) / 2 * number
+def finish_timer(start):
+    stop = time.perf_counter()
+    return stop-start
+
+def function_performence(func, arg):
+    start = time.perf_counter()
+    func(arg)
+    stop = time.perf_counter()
+    return stop - start
+
+# Przekazywanie funkcji do funkcji
+
+print(function_performence(sumuj_do, a))
+print(function_performence(sumuj_do2, a))
+print(function_performence(sumuj_do3, a))
+print(function_performence(sumuj_do4, a))
+print(function_performence(sumuj_do5, a))
