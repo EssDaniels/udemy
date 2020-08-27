956cece2b860b10a56d6bcffc6af1d7dfbba60dc
EssDaniels <skibol2@wp.pl>
2020-08-27 19:34:39 +0200

    kurs

---
+++ udemy/losowanie.py
@@ -0,0 +1,33 @@
+import random
+from collections import Counter
+
+
+
+
+
+
+
+
+def will_weapon_hit(weaponChanceToHitPercentage):
+    isHitChance = random.uniform(0, 100)
+    if (isHitChance < weaponChanceToHitPercentage):
+        return "hit"
+    else:
+        return "not hit"
+
+
+
+
+x = 0
+listHit = []
+
+while x < 100:
+    x = x + 1
+    listHit.append(will_weapon_hit(50))
+
+dictionaryHit = Counter(listHit)
+
+print(dictionaryHit)
+
+
+
